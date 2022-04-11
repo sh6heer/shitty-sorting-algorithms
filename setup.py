83216e7e6ebd5dac67df9a60a25ff531a70c208a
shaheer <shaheersohail2020@gmail.com>
2022-04-11 19:01:04 +0100

    added files to gitignore

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="shitsort",
-    version="1.0.3",
+    version="1.0.4",
     description="Really really bad sorting algorithms",
     long_description=README,
     long_description_content_type="text/markdown",
